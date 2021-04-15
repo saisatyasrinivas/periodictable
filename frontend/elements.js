da67c47cdb58ac6dcd23aab2fdc282d2f6e8db64
srinivas <srinivasgangineni18@gmail.com>
2021-04-15 10:53:12 -0400

    Adding event listener on sleect list

--- frontend/elements.js
+++ frontend/elements.js
@@ -5,27 +5,64 @@ $(document).ready(function(){
         var url2 = 'http://localhost:5000/periodictable/blocks/';
         var url3 = 'http://localhost:5000/periodictable/groups/';
         var url4 = 'http://localhost:5000/periodictable/periods/';
-        
-        callEndPoint(url, 'classification');
-        callEndPoint(url1, 'standardState');
-        callEndPoint(url2, 'block');
-        callEndPoint(url3, 'group');
-        callEndPoint(url4, 'period');
-        
+
+        $.ajax({
+            url: url,
+            type: 'GET',
+            success: function(response){
+                insert_html(response,'classification');
+                $.ajax({
+                    url: url1,
+                    type: 'GET',
+                    success: function(response){
+                        insert_html(response,'standardState');
+                        $.ajax({
+                            url: url2,
+                            type: 'GET',
+                            success: function(response){
+                                insert_html(response,'block');
+                                $.ajax({
+                                    url: url3,
+                                    type: 'GET',
+                                    success: function(response){
+                                        insert_html(response,'group');
+                                        $.ajax({
+                                            url: url4,
+                                            type: 'GET',
+                                            success: function(response){
+                                                insert_html(response,'period');   
+                                            }
+                                        })
+                                        
+                                    }
+                                })
+                                
+                            }
+                        })
+                        
+                        
+                    }
+                })
+
+            }
+        })
     },3000)
 })
+$(`#classification`).change(function(){
+    let text = $(`#classification`).val();
+    console.log(text);
+})
 
-function callEndPoint(url,id){
-    $.ajax({
-        url: url,
-        type: 'GET',
-        success: function(response){
-            let options = response.options;
-            let htmlCode = "";
-            for(let i of options){
-                htmlCode += `<option>${i}</option>`
-            }
-            $(`#${id}`).html(htmlCode);
-        }
-    })
+function change_html(id){
+    console.log($(`${id} option:selected`).text());
 }
+
+function insert_html(response, id){
+    let options = response.options;
+    let htmlCode = "";
+    for(let i of options){
+        htmlCode += `<option value="${i}">${i}</option>`
+    }
+    $(`#${id}`).html(htmlCode);
+}
+   
